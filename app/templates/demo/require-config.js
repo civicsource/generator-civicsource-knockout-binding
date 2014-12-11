cc21cba59d919bc0bc87ef3093dcaef154025f55
Brian Oline <brian.oline@archoninfosys.com>
2014-12-11 17:45:56 -0600

    fixing templates

--- app/templates/demo/require-config.js
+++ app/templates/demo/require-config.js
@@ -1,28 +1,24 @@
-var require = (function (root) {
 
-	var bowerPath = "../../bower_components/";
 
-	var cfg = {
-		baseUrl: "./",
-		paths: {
-			"jquery": "https://code.jquery.com/jquery-1.10.1",
-			"knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-debug",
-			"bootstrap": "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min",
-			"lodash": '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
-			"stringTemplateEngine": bowerPath + 'knockout-require-templates/stringTemplateEngine',
-			"template": bowerPath + 'knockout-require-templates/template',
-			"text": bowerPath + 'requirejs-text/text',
-		},
-		shim:{
-			"bootstrap": {
-				deps:["jquery"]
-			}
+var bowerPath = "../../../bower_components/";
+
+requirejs.config({
+	baseUrl: "./",
+	paths: {
+		"jquery": "https://code.jquery.com/jquery-1.10.1",
+		"knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-debug",
+		"bootstrap": "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min",
+		"lodash": '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
+		"stringTemplateEngine": bowerPath + 'knockout-require-templates/stringTemplateEngine',
+		"template": bowerPath + 'knockout-require-templates/template',
+		"text": bowerPath + 'requirejs-text/text',
+		"knockout.punches": bowerPath + "knockout.punches/knockout.punches"
+	},
+	shim: {
+		"bootstrap": {
+			deps: ["jquery"]
 		}
-	};
+	}
+});
 
-	return cfg;
-})(this);
-
-if (typeof (exports) === "object") {
-	module.exports = require;
-}
+require(['./main'], function () { });
