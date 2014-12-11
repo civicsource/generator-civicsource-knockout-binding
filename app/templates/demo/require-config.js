94979ae4eb3b0e3cbd9e7f8549e53b195582e490
Brian Oline <brian.oline@archoninfosys.com>
2014-12-11 17:27:59 -0600

    fixing demo files

--- app/templates/demo/require-config.js
+++ app/templates/demo/require-config.js
@@ -5,17 +5,17 @@ var require = (function (root) {
 	var cfg = {
 		baseUrl: "./",
 		paths: {
-			"jquery": "https://code.jquery.com/jquery-1.10.1.js",
-			"knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-debug.js",
+			"jquery": "https://code.jquery.com/jquery-1.10.1",
+			"knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-debug",
 			"bootstrap": "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min",
 			"lodash": '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
-			stringTemplateEngine: bowerPath + 'knockout-require-templates/stringTemplateEngine',
-			template: bowerPath + 'knockout-require-templates/template',
-			text: bowerPath + 'requirejs-text/text',
+			"stringTemplateEngine": bowerPath + 'knockout-require-templates/stringTemplateEngine',
+			"template": bowerPath + 'knockout-require-templates/template',
+			"text": bowerPath + 'requirejs-text/text',
 		},
 		shim:{
 			"bootstrap": {
-				deps:["jquery","css!https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css"]
+				deps:["jquery"]
 			}
 		}
 	};
